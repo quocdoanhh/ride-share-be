0c967ae01d37d424c4d0d3484513ed3c319e4481
quocdoanhh <hoang2doanh511@gmail.com>
2025-07-07 19:14:14 +0700

    oath + sso

--- config/services.php
+++ config/services.php
@@ -41,4 +41,9 @@ return [
         'from' => env('TWILIO_FROM'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 ];
